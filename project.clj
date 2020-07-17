04081b1fd425e345d943c095eb88cf469a9ab0fb
Circle CI <circleci@infrablocks.io>
2020-07-17 17:16:14 +0000

    Pre-release version 0.1.2-RC2 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus "0.1.2-RC1"
+(defproject io.logicblocks/cartus "0.1.2-RC2"
   :description "A structured logging abstraction with multiple backends."
   :url "https://github.com/logicblocks/cartus"
 
