49416ab690f014409bf8c6c317cbaa494dd22ca6
Toby Clemson <tobyclemson@gmail.com>
2020-07-18 18:09:44 +0100

    Add first pass of getting started guide.

--- README.md
+++ README.md
@@ -10,9 +10,9 @@ multiple backends, currently including:
   them.
 * A cambium implementation for logging out via SLF4J and logback.
 
-Heavily inspired by [JUXT's blog on logging](https://juxt.pro/blog/logging).
+Heavily inspired by [JUXT's blog post on logging](https://juxt.pro/blog/logging).
 
-## Install
+## Installation
 
 Add the following to your `project.clj` file:
 
--- project.clj
+++ project.clj
@@ -55,10 +55,12 @@
    {:release-tasks
     [["shell" "git" "diff" "--exit-code"]
      ["change" "version" "leiningen.release/bump-version" "release"]
-     ["codox"]
      ["changelog" "release"]
      ["shell" "sed" "-E" "-i.bak" "s/\"[0-9]+\\.[0-9]+\\.[0-9]+\"/\"${:version}\"/g" "README.md"]
      ["shell" "rm" "-f" "README.md.bak"]
+     ["shell" "sed" "-E" "-i.bak" "s/\"[0-9]+\\.[0-9]+\\.[0-9]+\"/\"${:version}\"/g" "docs/getting-started.md"]
+     ["shell" "rm" "-f" "getting-started.md.bak"]
+     ["codox"]
      ["shell" "git" "add" "."]
      ["vcs" "commit" "Release version %s [skip ci]"]
      ["vcs" "tag"]
