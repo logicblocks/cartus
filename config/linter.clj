d565afeb7da04b4bc69a3df460e3c7f2ce99e159
Toby Clemson <tobyclemson@gmail.com>
2020-07-21 19:57:45 +0100

    Remove unused statement from linter config.

--- config/linter.clj
+++
@@ -1,7 +0,0 @@
-(disable-warning
-  {:linter :suspicious-expression
-   :for-macro 'clojure.core/or
-   :if-inside-macroexpansion-of #{'clojure.core.async/go
-                                  'clojure.core.async/alt!
-                                  'clojure.core.async/alt!!}
-   :within-depth 18})
