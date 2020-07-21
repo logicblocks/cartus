ecd704eb3d285adf44d1a9ee08e8c1fa5ea04b3f
Circle CI <circleci@infrablocks.io>
2020-07-21 08:20:43 +0000

    Pre-release version 0.1.7-RC2 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus "0.1.7-RC1"
+(defproject io.logicblocks/cartus "0.1.7-RC2"
   :description "Parent for all cartus modules."
 
   :plugins [[lein-modules "0.3.11"]
--- test/project.clj
+++ test/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus.test "0.1.7-RC1"
+(defproject io.logicblocks/cartus.test "0.1.7-RC2"
   :description "A test backend for cartus."
 
   :plugins  [[lein-modules "0.3.11"]]
