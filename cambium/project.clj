fee1d4d1a9f50c428f39269ce15084da264db80c
Circle CI <circleci@infrablocks.io>
2020-07-21 00:26:24 +0000

    Pre-release version 0.1.6-RC3 [skip ci]

--- cambium/project.clj
+++ cambium/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus.cambium "0.1.6-RC2"
+(defproject io.logicblocks/cartus.cambium "0.1.6-RC3"
   :description "A structured logging abstraction with multiple backends."
 
   :plugins  [[lein-modules "0.3.11"]
