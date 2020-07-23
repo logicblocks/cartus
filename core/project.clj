1d96c3e1c7e146faaddc2b70b7008e1acfac9611
Circle CI <circleci@infrablocks.io>
2020-07-23 10:57:05 +0000

    Pre-release version 0.1.8-RC2 [skip ci]

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus.core "0.1.8-RC1"
+(defproject io.logicblocks/cartus.core "0.1.8-RC2"
   :description "A structured logging abstraction with multiple backends."
 
   :plugins  [[lein-modules "0.3.11"]])
