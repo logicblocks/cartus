a9eb552091ed7b96b18521eb4ae5cd6a5c070984
Toby Clemson <tobyclemson@gmail.com>
2020-07-24 15:01:36 +0100

    Bump version.

--- cambium/project.clj
+++ cambium/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus.cambium "0.1.11-RC5"
+(defproject io.logicblocks/cartus.cambium "0.1.12-RC1"
   :description "A cambium backend for cartus."
 
   :plugins  [[lein-modules "0.3.11"]]
