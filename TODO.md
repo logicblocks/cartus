42647ccd0335ed02dcd6465e906adfcf3ea477d5
Toby Clemson <tobyclemson@gmail.com>
2020-08-10 14:00:00 +0100

    Simplify and improve performance of subsequences matcher.

--- TODO.md
+++ TODO.md
@@ -3,3 +3,4 @@ ToDo
 
 * Add custom assertions for clojure.test
 * Add re-typing and re-levelling transformations.
+* Maintain sequence type in subsequences matcher.
--- test/src/cartus/test/matchers.clj
+++ test/src/cartus/test/matchers.clj
@@ -3,7 +3,7 @@
    [clojure.tools.trace :as trace]
    [clojure.math.combinatorics :as comb]
 
-   [matcher-combinators.core :as mc-core]
+   [matcher-combinators.core :as mc]
    [matcher-combinators.model :as mc-model]
    [matcher-combinators.result :as mc-result])
   (:import
@@ -16,12 +16,9 @@
    (validate-input expected actual pred pred matcher-name type))
   ([expected actual expected-pred actual-pred matcher-name type]
    (cond
-     (= actual ::mc-core/missing)
+     (= actual ::mc/missing)
      {::mc-result/type   :mismatch
-      ::mc-result/value  (mc-model/->Missing
-                           (if (fn? expected)
-                             (str "predicate: " expected)
-                             expected))
+      ::mc-result/value  (mc-model/->Missing expected)
       ::mc-result/weight 1}
 
      (not (expected-pred expected))
@@ -50,120 +47,107 @@
     (comb/combinations elements n)
     (for [i (range 1 (inc n))]
       (mapcat
-        #(for [ps (apply comb/cartesian-product
-                    (repeat i (range n)))
-               :when (apply < ps)]
-           (reduce (fn [c p] (insert c p ::missing)) % ps))
+        #(for [ps (comb/combinations (range n) i)]
+           (reduce (fn [c p] (insert c p ::mc/missing)) % ps))
         (comb/combinations elements (- n i))))))
 
 (defn match-candidate-result [match-candidate matchers]
-  (trace/trace "match-candidate" match-candidate)
-  (let [results
-        (map
-          (fn [matcher element]
-            (trace/trace "matcher" matcher)
-            (trace/trace "element" element)
-            {:matcher matcher
-             :element element
-             :result  (if (= ::missing element)
-                        {::mc-result/type   :mismatch
-                         ::mc-result/value  (mc-model/->Missing
-                                              (if (fn? matcher)
-                                                (str "predicate: " matcher)
-                                                matcher))
-                         ::mc-result/weight 1}
-                        (mc-core/match matcher element))})
-          matchers
-          match-candidate)
-        result
+  (reduce
+    (fn [{::mc-result/keys [type value elements matched weight]}
+         [matcher element]]
+      (let [{element-value  ::mc-result/value
+             element-weight ::mc-result/weight
+             :as            element-result}
+            (mc/match matcher element)
+
+            type (if (mc/indicates-match? element-result) type :mismatch)
+            value (concat value [element-value])
+            elements (concat elements [element])
+            matched (if (mc/indicates-match? element-result)
+                      (concat matched [element])
+                      matched)
+            weight (+ weight element-weight)]
+        #::mc-result{:type     type
+                     :value    value
+                     :elements elements
+                     :matched  matched
+                     :weight   weight}))
+    #::mc-result{:type     :match
+                 :value    (empty matchers)
+                 :elements (empty matchers)
+                 :matched  (empty matchers)
+                 :weight   0}
+    (map vector matchers match-candidate)))
+
+(defn format-mismatch [result elements]
+  (let [{:keys [value remaining]}
         (reduce
-          (fn [overall-result {:keys [matcher element result]}]
-            {::mc-result/type     (if (= (::mc-result/type overall-result)
-                                        :mismatch)
-                                    :mismatch
-                                    (::mc-result/type result))
-             ::mc-result/value    (concat
-                                    (::mc-result/value overall-result)
-                                    [(::mc-result/value result)])
-             ::mc-result/matchers (concat
-                                    (::mc-result/matchers overall-result)
-                                    [matcher])
-             ::mc-result/elements (concat
-                                    (::mc-result/elements overall-result)
-                                    [element])
-             ::mc-result/weight   (+
-                                    (::mc-result/weight overall-result)
-                                    (::mc-result/weight result))})
-          {::mc-result/type     :match
-           ::mc-result/value    (empty matchers)
-           ::mc-result/matchers (empty matchers)
-           ::mc-result/elements (empty matchers)
-           ::mc-result/weight   0}
-          results)]
-    result))
+          (fn [{:keys [remaining value]} element]
+            (let [[missing [next]]
+                  (split-with #(instance? Missing (first %)) remaining)]
+              (cond
+                (= element (second next))
+                {:remaining (drop (inc (count missing)) remaining)
+                 :value     (concat value (map first missing)
+                              [(first next)])}
+
+                (instance? Mismatch (first next))
+                {:remaining (drop (inc (count missing)) remaining)
+                 :value     (concat value (map first missing)
+                              [(first next)])}
+
+                :else
+                {:remaining remaining
+                 :value     (concat value [(->Ignored element)])})))
+          {:remaining (map vector
+                        (::mc-result/value result)
+                        (::mc-result/elements result))
+           :value     []}
+          elements)]
+    #::mc-result{:type   :mismatch
+                 :value  (concat value (map first remaining))
+                 :weight (::mc-result/weight result)}))
+
+(defn- better-mismatch? [best candidate]
+  (let [best-matched (-> best ::mc-result/matched count)
+        candidate-matched (-> candidate ::mc-result/matched count)
+        candidate-weight (::mc-result/weight candidate)
+        best-weight (::mc-result/weight best)]
+    (and (>= candidate-matched best-matched)
+      (< candidate-weight best-weight))))
 
 (defn match-subsequence [matchers elements]
-  (let [match-candidates (match-candidates elements (count matchers))
-        match-results
-        (map (fn [c]
-               (let [r (match-candidate-result c matchers)]
-                 (trace/trace "match-result" r)
-                 r))
-          match-candidates)
-        match
-        (first
-          (filter
-            #(= (::mc-result/type %) :match)
-            match-results))]
-    (trace/trace "match" match)
-    (or match
-      (let [best-mismatch
-            (first (sort-by ::mc-result/weight match-results))
-            _ (trace/trace "best-mismatch" best-mismatch)
-            {:keys [value remaining]}
-            (reduce
-              (fn [{:keys [remaining value]} element]
-                (let [[missing [next]]
-                      (split-with #(instance? Missing (first %)) remaining)]
-                  (trace/trace "--------------" nil)
-                  (trace/trace "remaining" remaining)
-                  (trace/trace "value" value)
-                  (trace/trace "element" element)
-                  (trace/trace "missing" missing)
-                  (trace/trace "next" next)
-                  (cond
-                    (= element (second next))
-                    {:remaining (drop (inc (count missing)) remaining)
-                     :value     (concat value (map first missing)
-                                  [(first next)])}
+  (let [result
+        (reduce
+          (fn [best candidate]
+            (let [result (match-candidate-result candidate matchers)]
+              (cond
+                (mc/indicates-match? result) (reduced ::mc/match-found)
+                (better-mismatch? best result) result
+                :else best)))
+          #::mc-result{:type     :mismatch
+                       :value    (empty matchers)
+                       :elements (empty matchers)
+                       :matched  (empty matchers)
+                       :weight   Integer/MAX_VALUE}
+          (match-candidates elements (count matchers)))]
+    (if (= ::mc/match-found result)
+      #::mc-result{:type   :match
+                   :value  elements
+                   :weight 0}
+      (format-mismatch result elements))))
 
-                    (instance? Mismatch (first next))
-                    {:remaining (drop (inc (count missing)) remaining)
-                     :value     (concat value (map first missing)
-                                  [(first next)])}
-
-                    :else
-                    {:remaining remaining
-                     :value     (concat value [(->Ignored element)])})))
-              {:remaining (map vector
-                            (::mc-result/value best-mismatch)
-                            (::mc-result/elements best-mismatch))
-               :value     []}
-              elements)]
-        {::mc-result/type   :mismatch
-         ::mc-result/value  (concat value (map first remaining))
-         ::mc-result/weight (::mc-result/weight best-mismatch)}))))
-
-(defrecord SubsequencesSeq [expected]
-           mc-core/Matcher
-           (-matcher-for [this] this)
-           (-matcher-for [this _] this)
-           (-match [_ actual]
-             (if-let [issue
-                      (validate-input
-                        expected actual sequential? 'subsequences "sequential")]
-               issue
-               (match-subsequence expected actual))))
+(defrecord SubsequencesSeq
+  [expected]
+  mc/Matcher
+  (-matcher-for [this] this)
+  (-matcher-for [this _] this)
+  (-match [_ actual]
+    (if-let [issue
+             (validate-input
+               expected actual sequential? 'subsequences "sequential")]
+      issue
+      (match-subsequence expected actual))))
 
 (defn subsequences
   "Matcher that will match when a sequence contains each of the expected items
@@ -171,4 +155,4 @@
   [expected]
   (cond
     (sequential? expected) (->SubsequencesSeq expected)
-    :else (mc-core/->InvalidType expected "subsequences" "seq")))
+    :else (mc/->InvalidType expected "subsequences" "seq")))
