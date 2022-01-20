d94421d2d1aeaa06e528221cbea1e380dde38a89
Circle CI <circleci@infrablocks.io>
2022-01-20 15:06:03 +0000

    Pre-release version 0.1.16-RC1 [skip ci]

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus.core "0.1.15"
+(defproject io.logicblocks/cartus.core "0.1.16-RC1"
   :description "A structured logging abstraction with multiple backends."
 
   :plugins  [[lein-modules "0.3.11"]])
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus "0.1.15"
+(defproject io.logicblocks/cartus "0.1.16-RC1"
   :description "Parent for all cartus modules."
 
   :plugins [[lein-modules "0.3.11"]
