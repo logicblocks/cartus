580c50c2f407758ef6da3a5cb4a4700274bcda05
Circle CI <circleci@infrablocks.io>
2022-06-15 09:10:46 +0000

    Pre-release version 0.1.18-RC2 [skip ci]

--- test/project.clj
+++ test/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus.test "0.1.18-RC1"
+(defproject io.logicblocks/cartus.test "0.1.18-RC2"
   :description "A test backend for cartus."
 
   :plugins  [[lein-modules "0.3.11"]]
