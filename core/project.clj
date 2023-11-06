451bcd4433711183aa7b68b42e6fd957aefa8f33
Circle CI <circleci@logicblocks.io>
2023-11-06 23:14:47 +0000

    Pre-release version 0.1.19-RC4 [skip ci]

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus.core "0.1.19-RC3"
+(defproject io.logicblocks/cartus.core "0.1.19-RC4"
   :description "A structured logging abstraction with multiple backends."
 
   :plugins  [[lein-modules "0.3.11"]])
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus "0.1.19-RC3"
+(defproject io.logicblocks/cartus "0.1.19-RC4"
   :description "Parent for all cartus modules."
 
   :plugins [[lein-modules "0.3.11"]
