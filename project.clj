654f3f17f6346c3196b470c5f0c6bbda6c72acdf
Circle CI <circleci@logicblocks.io>
2024-06-06 11:49:03 +0000

    Pre-release version 0.1.19-RC6 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus "0.1.19-RC5"
+(defproject io.logicblocks/cartus "0.1.19-RC6"
   :description "Parent for all cartus modules."
 
   :plugins [[lein-modules "0.3.11"]
