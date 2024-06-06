af36a04af4c19ec4243e7e61beccf8e95865beee
Circle CI <circleci@logicblocks.io>
2024-06-06 14:37:59 +0000

    Pre-release version 0.1.19-RC7 [skip ci]

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus.core "0.1.19-RC6"
+(defproject io.logicblocks/cartus.core "0.1.19-RC7"
   :description "A structured logging abstraction with multiple backends."
 
   :plugins  [[lein-modules "0.3.11"]])
--- redaction/project.clj
+++ redaction/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus.redaction "0.1.19-RC6"
+(defproject io.logicblocks/cartus.redaction "0.1.19-RC7"
   :description "A redaction transformer for cartus."
 
   :plugins  [[lein-modules "0.3.11"]]
