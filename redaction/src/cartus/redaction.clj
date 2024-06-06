3eb96ec8514e382d4bfb94e43cd55840e5bf724b
John Cowie Del Corral <john@go-atomic.io>
2024-06-06 13:34:41 +0200

    [#LGBK-31] Implement with-redaction

--- redaction/src/cartus/redaction.clj
+++ redaction/src/cartus/redaction.clj
@@ -1,4 +1,46 @@
-(ns cartus.redaction)
+(ns cartus.redaction
+  (:require [clojure.walk :as walk]
+            [cartus.core :as log]))
 
-(defn with-redaction [logger _redact-options]
-  logger)
+(defn redact
+  [m {:keys [redact-value-fn]}]
+  (walk/postwalk
+    (fn [x]
+      (if (map? x)
+        (->> x
+          (map (fn [[k v]]
+                 (if-let [redaction (redact-value-fn k v)]
+                   [k redaction]
+                   [k v])))
+          (into {}))
+        x))
+    m))
+
+(def default-redaction-options
+  {:redact-value-fn
+   (fn [k _v]
+     (when (#{:authorization :password :token
+              :secret :secret-key :secret-token} (keyword k))
+       "[REDACTED]"))})
+
+(defn with-redaction
+  "Applys a transformation to a logger to redact sensitive values from
+   log context.
+
+   arity-1 version expects a cartus logger to transform
+   arity-2 version expects a cartus logger plus override redact-options
+
+   redact-options:
+
+   `{:redact-value-fn redact-value-fn}`
+
+   Where `redact-value-fn` takes two arguments, the key and value at
+   a specific point in a data structure during traversal.  If the function
+   returns a value, that value is used as a substitute for the existing value.
+  "
+  ([logger]
+   (with-redaction logger default-redaction-options))
+  ([logger redact-options]
+   (log/with-transformation
+     logger
+     (map #(update % :context redact redact-options)))))
--- redaction/test/cartus/redaction_test.clj
+++ redaction/test/cartus/redaction_test.clj
@@ -1,7 +1,55 @@
 (ns cartus.redaction-test
   (:require
    [clojure.test :refer :all]
-   [cartus.test :as log-test :refer [logged?]]))
+   [clojure.string :as str]
+   [cartus.redaction :as sut]
+   [cartus.core :as log]
+   [cartus.test :as test-log :refer [logged?]]))
 
 (deftest with-redaction-test
-  (testing "can transform logger to redact keys"))
+  (testing "can transform logger to redact sensitive keys"
+    (let [logger (test-log/logger)
+          redacted-logger (sut/with-redaction logger)]
+      (log/info
+        redacted-logger
+        ::something-happened
+        {:data          {:token        "secret-token"
+                         :secret-token "super-secret-token"}
+         :passwords     [{:password "password1"}
+                         {:password "password2"}]
+         :secret        "birds aren't real"
+         :authorization "authorization-deets"
+         :secret-key    "G flat"
+         :other         "some other value"})
+      (is (logged?
+            logger
+            #{:once}
+            {:type    ::something-happened
+             :context {:data          {:token        "[REDACTED]"
+                                       :secret-token "[REDACTED]"}
+                       :passwords     [{:password "[REDACTED]"}
+                                       {:password "[REDACTED]"}]
+                       :secret        "[REDACTED]"
+                       :authorization "[REDACTED]"
+                       :secret-key    "[REDACTED]"
+                       :other         "some other value"}}))))
+
+  (testing "can override function for determining how/when to redact"
+    (let [logger (test-log/logger)
+          options {:redact-value-fn (fn [_k v]
+                                      (when (-> v
+                                              str/lower-case
+                                              (str/includes? "voldemort"))
+                                        "He who must not be named"))}
+          redacted-logger (sut/with-redaction logger options)]
+      (log/info redacted-logger ::a-log {:character "Harry Potter"})
+      (log/info redacted-logger ::a-log {:character "Hagrid"})
+      (log/info redacted-logger ::a-log {:character "Lord Voldemort"})
+      (log/info redacted-logger ::a-log {:character "Hermione Granger"})
+      (is (logged?
+            logger
+            #{:once}
+            {:context {:character "Harry Potter"}}
+            {:context {:character "Hagrid"}}
+            {:context {:character "He who must not be named"}}
+            {:context {:character "Hermione Granger"}})))))
