afc7a7d8afcba6313e6e8a42c8faa363613967eb
Circle CI <circleci@logicblocks.io>
2024-06-10 09:36:13 +0000

    Pre-release version 0.1.19-RC11 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus "0.1.19-RC10"
+(defproject io.logicblocks/cartus "0.1.19-RC11"
   :description "Parent for all cartus modules."
 
   :plugins [[lein-modules "0.3.11"]
--- redaction/project.clj
+++ redaction/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/cartus.redaction "0.1.19-RC10"
+(defproject io.logicblocks/cartus.redaction "0.1.19-RC11"
   :description "A redaction transformer for cartus."
 
   :plugins  [[lein-modules "0.3.11"]]
