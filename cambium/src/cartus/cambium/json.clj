af23ba6aa7b2a5aac451b5a65fb3c2010337716d
John Cowie Del Corral <john@go-atomic.io>
2024-06-10 11:29:46 +0200

    [#LGBK-31] Add encodings for Duration and Period
    Co-authored-by: belen-m-h <belen@go-atomic.io>

--- cambium/src/cartus/cambium/json.clj
+++ cambium/src/cartus/cambium/json.clj
@@ -1,10 +1,20 @@
 (ns cartus.cambium.json
   (:require [cheshire.generate :as cg])
   (:import [com.fasterxml.jackson.core JsonGenerator]
-           [java.time Instant]))
+           [java.time Instant Duration Period]))
 
 (defn add-java-time-encoders! []
   (cg/add-encoder Instant
+    (fn [value jsonGenerator]
+      (.writeString
+        ^JsonGenerator jsonGenerator
+        ^String (str value))))
+  (cg/add-encoder Duration
+    (fn [value jsonGenerator]
+      (.writeString
+        ^JsonGenerator jsonGenerator
+        ^String (str value))))
+  (cg/add-encoder Period
     (fn [value jsonGenerator]
       (.writeString
         ^JsonGenerator jsonGenerator
--- cambium/test/cartus/cambium/json_test.clj
+++ cambium/test/cartus/cambium/json_test.clj
@@ -10,7 +10,7 @@
             [cheshire.core :as json])
   (:import
    (java.time
-     Instant)
+     Instant Period Duration)
    (java.util
      UUID)))
 
@@ -36,6 +36,8 @@
               (throw (Exception. "You shouldn't have called me!"))))
           context {:a-uuid       (UUID/fromString a-uuid-str)
                    :an-instant   (Instant/ofEpochMilli 0)
+                   :a-duration (Duration/ofDays 4)
+                   :a-period (Period/ofWeeks 2)
                    :unencodeable unencodable}]
       (cartus-cambium/initialise)
 
@@ -43,6 +45,8 @@
 
       (is (= [{:a-uuid       "ae355a31-a07b-44f0-9d3f-6dc2284da37f"
                :an-instant   "1970-01-01T00:00:00Z"
+               :a-duration "PT96H"
+               :a-period     "P14D"
                :unencodeable "Unable to encode MDC value as JSON"}]
             (->> (log-lines log-output-stream)
               (map #(select-keys % (keys context)))))))))
@@ -56,6 +60,8 @@
           a-uuid-str "ae355a31-a07b-44f0-9d3f-6dc2284da37f"
           context {:a-nested-map
                    {:a-uuid     (UUID/fromString a-uuid-str)
+                    :a-duration (Duration/ofDays 1)
+                    :a-period (Period/ofMonths 3)
                     :an-instant (Instant/ofEpochMilli 0)}}]
       (cartus-cambium/initialise)
 
@@ -63,6 +69,8 @@
 
       (is (= [{:a-nested-map
                {:a-uuid     "ae355a31-a07b-44f0-9d3f-6dc2284da37f"
+                :a-duration "PT24H"
+                :a-period   "P3M"
                 :an-instant "1970-01-01T00:00:00Z"}}]
             (->> (log-lines log-output-stream)
               (map #(select-keys % (keys context)))))))))
